962512cc87b6e69c620be435da67fc6e16fa61b2
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-10-20 16:09:23 +0200

    :sparkles: (logout) redirect after logout

--- src/components/auth/LogoutButton.jsx
+++ src/components/auth/LogoutButton.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// Hooks
+import { useHistory } from 'react-router-dom'
+
 // Contexts
 import { useAuthDispatch } from '../../contexts/auth'
 import { logoutAction } from '../../contexts/auth/actions'
@@ -9,9 +12,13 @@ import { Button } from '@heetch/flamingo-react'
 
 export default () => {
   const authDispatch = useAuthDispatch()
+  const history = useHistory()
 
   const handleLogout = async () => {
     await logoutAction(authDispatch)
+
+    // Redirect after logout
+    history.push('/')
   }
 
   return (
