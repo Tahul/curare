6a42f07ba54110a241b10263db30e2eab93b7657
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-10-20 15:59:04 +0200

    :bug: (auth) fix sanctum auth by proxying domain requests

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
   mount: {
     public: '/',
@@ -6,5 +8,16 @@ module.exports = {
   plugins: [
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
+    [
+      '@snowpack/plugin-run-script',
+      {
+        cmd: "eslint 'src/**/*.{js,jsx,ts,tsx}'",
+        // Optional: Use npm package "watch" to run on every file change
+        watch: 'watch "$1" src',
+      },
+    ],
   ],
-};
+  proxy: {
+    '/api': process.env.SNOWPACK_PUBLIC_API_URL,
+  },
+}
