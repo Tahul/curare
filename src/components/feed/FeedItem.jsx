92d4d77d05164983a0284eec55656f81a10ee78b
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-16 19:26:45 +0100

    :sparkles: (feed) improve feeditem display

--- src/components/feed/FeedItem.jsx
+++ src/components/feed/FeedItem.jsx
@@ -1,4 +1,4 @@
-import { theme, UiText } from '@heetch/flamingo-react'
+import { Icon, Text, theme, UiText } from '@heetch/flamingo-react'
 import React from 'react'
 import styled from 'styled-components'
 import LinkItem from '../links/LinkItem'
@@ -6,35 +6,73 @@ import Fill from '../../assets/images/fill.png'
 import { Img } from 'react-image'
 import { useInView } from 'react-intersection-observer'
 import { motion, useAnimation } from 'framer-motion'
+import { useHistory } from 'react-router-dom'
+import { formatDistanceToNow } from 'date-fns'
 
 const StyledFeedItem = styled.div`
   margin-top: ${theme.space.xxl};
 
   .top-line {
+    position: relative;
     display: flex;
     align-items: center;
     margin-bottom: ${theme.space.l};
+    cursor: pointer;
+
+    .open-arrow {
+      position: absolute;
+      top: 50%;
+      right: 0;
+      transform: translateY(-50%);
+    }
 
     img {
-      height: 2rem;
-      width: 2rem;
-      margin-right: ${theme.space.m};
+      height: 3rem;
+      width: 3rem;
+      border-radius: 50%;
+    }
+
+    .data {
+      display: flex;
+      flex-direction: column;
+
+      p {
+        margin-left: ${theme.space.m};
+
+        &:nth-child(3) {
+          font-size: ${theme.fontSize.s};
+        }
+      }
     }
   }
 `
 
+const randomIntFromInterval = (min, max) => {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 const FeedItem = ({ item }) => {
+  const history = useHistory()
   const controls = useAnimation()
+  const [hover, setHover] = React.useState(false)
   const [ref, inView] = useInView({
     triggerOnce: true,
     rootMargin: '-20px 0px',
   })
+  const fullName = `${item.profile.first_name || ''}${
+    item.profile.last_name ? ' ' + item.profile.last_name : ''
+  }`
 
   React.useEffect(() => {
     if (inView) {
       controls.start('visible')
     }
   }, [controls, inView])
+
+  const handleProfileOpen = () => {
+    history.push(`/profile/${item.profile.name}`)
+  }
 
   return (
     <StyledFeedItem ref={ref}>
@@ -45,17 +83,46 @@ const FeedItem = ({ item }) => {
         variants={{
           hidden: {
             opacity: 0,
-            x: -50,
+            x: randomIntFromInterval(-100, 100),
+            rotate: randomIntFromInterval(-20, 0),
+            y: randomIntFromInterval(20, 0),
           },
           visible: {
             opacity: 1,
             x: 0,
+            rotate: 0,
+            y: 0,
           },
         }}
+        onClick={handleProfileOpen}
+        onMouseOver={() => setHover(true)}
+        onMouseOut={() => setHover(false)}
       >
-        <Img src={item.profile.avatar_ur || Fill} />
+        <Img src={item.profile.avatar_url || Fill} />
 
-        <UiText variant="contentBold">@{item.profile.name}</UiText>
+        <div className="data">
+          <UiText
+            variant="contentBold"
+            alt={`@${item.profile.name}`}
+            title={`@${item.profile.name}`}
+          >
+            {fullName && fullName !== '' ? fullName : `@${item.profile.name}`}
+          </UiText>
+
+          <Text>{formatDistanceToNow(new Date(item.created_at))}</Text>
+
+          <div className="open-arrow">
+            <motion.div
+              animate={
+                hover
+                  ? { opacity: 1, x: 0 }
+                  : { opacity: 0, x: -100, transition: { duration: 0.05 } }
+              }
+            >
+              <Icon icon="IconArrowRight" size="l" />
+            </motion.div>
+          </div>
+        </div>
       </motion.div>
 
       <LinkItem key={item.id} link={item} editable={false} />
