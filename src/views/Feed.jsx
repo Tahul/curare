c9b829fcb20efd57e7070ca546eb74f64dd3368e
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-18 03:36:15 +0100

    :bug: (feed) fix wrong space method

--- src/views/Feed.jsx
+++ src/views/Feed.jsx
@@ -37,7 +37,7 @@ const Feed = () => {
               onLoad={handleNextPage}
             >
               <Text>
-                You reached the end of your feed{' '}
+                You reached the end of your feed&nbsp;
                 <span role="img" alt="Confettis">
                   🎉
                 </span>
