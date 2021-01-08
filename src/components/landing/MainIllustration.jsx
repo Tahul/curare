7c094c8d6fa0eec7d1bda3d9dd00891394a1fa7e
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-08 20:00:05 +0100

    :sparkles: (landing) add bush to main illustration

--- src/components/landing/MainIllustration.jsx
+++ src/components/landing/MainIllustration.jsx
@@ -1,6 +1,7 @@
 import { theme } from '@heetch/flamingo-react'
 import React from 'react'
 import styled from 'styled-components'
+import Bush from './illustrations/Bush'
 import Character from './illustrations/Character'
 import Heart from './illustrations/Heart'
 import Phone from './illustrations/Phone'
@@ -21,14 +22,14 @@ const StyledMainIllustration = styled.div`
 const MainIllustration = () => {
   return (
     <StyledMainIllustration>
+      <Bush style={{ height: '320px' }} transition={{ delay: 0.2 }} />
+
       <Phone
         style={{ right: '0px', top: '20px', height: '300px' }}
-        transition={{ delay: 0.2 }}
+        transition={{ delay: 0.3 }}
       />
 
-      <Heart
-        style={{ height: '25px', width: '25px', left: '40px', zIndex: '120' }}
-      />
+      <Heart style={{ height: '25px', width: '25px', left: '40px' }} />
 
       <Character
         style={{ left: '-15px', height: '320px' }}
