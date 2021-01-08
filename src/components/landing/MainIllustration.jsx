6c121d1a12cf9a7f1cd754fe3e0e876db796ffbf
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-08 19:31:48 +0100

    :sparkles: (landing) implement main illustration component

---
+++ src/components/landing/MainIllustration.jsx
@@ -0,0 +1,41 @@
+import { theme } from '@heetch/flamingo-react'
+import React from 'react'
+import styled from 'styled-components'
+import Character from './illustrations/Character'
+import Heart from './illustrations/Heart'
+import Phone from './illustrations/Phone'
+
+const StyledMainIllustration = styled.div`
+  margin-top: ${theme.space.xxl};
+  position: relative;
+  width: 100%;
+  height: 380px;
+  overflow: visible;
+
+  svg,
+  div {
+    position: absolute;
+  }
+`
+
+const MainIllustration = () => {
+  return (
+    <StyledMainIllustration>
+      <Phone
+        style={{ right: '0px', top: '20px', height: '360px' }}
+        transition={{ delay: 0.2 }}
+      />
+
+      <Heart
+        style={{ height: '25px', width: '25px', left: '40px', zIndex: '120' }}
+      />
+
+      <Character
+        style={{ left: '-15px', height: '380px' }}
+        transition={{ delay: 0.1 }}
+      />
+    </StyledMainIllustration>
+  )
+}
+
+export default MainIllustration
