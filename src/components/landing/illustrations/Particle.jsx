dba72c574ee69fc5ba145cf313f72a3cc3c97338
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-12 23:16:30 +0100

    :sparkles: (particles) remove emojis from particle component, prepare for svg icons

--- src/components/landing/illustrations/Particle.jsx
+++ src/components/landing/illustrations/Particle.jsx
@@ -56,9 +56,24 @@ const Particle = ({ particle = 'book', children, style, ...rest }) => {
       style={style}
       {...rest}
     >
-      <text fontSize="2em" text-anchor="middle" x="50%" y="20%" dy=".25em">
-        {children}
-      </text>
+      {particle === 'podcast' && (
+        <>
+          <path
+            fill="#9fa8da"
+            d="M33,24c0,3.3-2.7,6-6,6h-6c-3.3,0-6-2.7-6-6V10c0-3.3,2.7-6,6-6h6c3.3,0,6,2.7,6,6V24z"
+          />
+          <path fill="#3f51b5" d="M22 34H26V44H22z" />
+          <path fill="#303f9f" d="M35,43c0,0-6-2-11-2s-11,2-11,2v2h22V43z" />
+          <path
+            fill="#5c6bc0"
+            d="M15 10H23V12H15zM25 10H33V12H25zM25 14H33V16H25zM15 14H23V16H15zM25 18H33V20H25zM15 18H23V20H15zM15 22H23V24H15zM25 22H33V24H25z"
+          />
+          <path
+            fill="#3f51b5"
+            d="M26,35h-4c-3.1,0-9-1.7-9-8v-9h2v9c0,5.9,6.7,6,7,6h4c0.3,0,7-0.1,7-6v-9h2v9C35,33.3,29.1,35,26,35z"
+          />
+        </>
+      )}
     </motion.svg>
   )
 }
